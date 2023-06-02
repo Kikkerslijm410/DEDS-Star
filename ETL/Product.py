0ad408a67e6c2b85ef53c6f2c7af506633cd687a
Kikkerslijm410 <Lucasclavel356@gmail.com>
2023-06-02 13:56:57 +0200

    highest number needs to be used

--- ETL/Product.py
+++ ETL/Product.py
@@ -10,7 +10,7 @@ conn = pyodbc.connect(conn_str)
 cursor = conn.cursor()
 
 query = '''
-SELECT product.id, product.name, product.description, product.Category
+SELECT product.name, product.category
 FROM product;
 '''
 cursor.execute(query)
@@ -20,9 +20,11 @@ results = cursor.fetchall()
 if not os.path.exists('Output'):
     os.makedirs('Output')
 
-with open('Output/Product.csv', 'w', newline='', encoding='utf-16') as csvfile:
-    fieldnames = ['ID', 'Name', 'Description', 'Category']
+with open('Output/Product2.csv', 'w', newline='', encoding='utf-16') as csvfile:
+    fieldnames = ['ProductID', 'Name', 'Category']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
+    x = 1
     for row in results:
-        writer.writerow({'ID': row[0], 'Name': row[1], 'Description': row[2], 'Category': row[3]})
+        writer.writerow({'ProductID': x, 'Name': row[0], 'Category': row[1]})
+        x += 1
