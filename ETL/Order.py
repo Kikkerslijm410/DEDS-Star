01ddfd71a21a039c9b9b5188f1b4190ce0256b0d
Kikkerslijm410 <Lucasclavel356@gmail.com>
2023-06-06 15:53:37 +0200

    added adress

--- ETL/Order.py
+++ ETL/Order.py
@@ -10,9 +10,8 @@ conn = pyodbc.connect(conn_str)
 cursor = conn.cursor()
 
 query = '''
-SELECT sales_order.id, product.quantity, product.unit_price, sales_order.order_date, customer.id, product.id
-FROM customer INNER JOIN (product INNER JOIN (sales_order INNER JOIN sales_order_item ON sales_order.id = sales_order_item.id) ON product.id = sales_order_item.prod_id) ON customer.id = sales_order.cust_id;
-'''
+SELECT sales_order.id, product.quantity, product.unit_price, sales_order.order_date, customer.id, product.id, customer.address
+FROM product INNER JOIN ((customer INNER JOIN sales_order ON customer.id = sales_order.cust_id) INNER JOIN sales_order_item ON sales_order.id = sales_order_item.id) ON product.id = sales_order_item.prod_id;'''
 cursor.execute(query)
 results = cursor.fetchall()
 
@@ -21,8 +20,8 @@ if not os.path.exists('ETL/Output'):
     os.makedirs('ETL/Output')
 
 with open('ETL/Output/Order.csv', 'w', newline='', encoding='utf-16') as csvfile:
-    fieldnames = ['ID', 'Quantity', 'Unit_price', 'Order_date', 'Customer', 'Product']
+    fieldnames = ['ID', 'Quantity', 'Unit_price', 'Order_date', 'Customer', 'Product', 'Address']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     for row in results:
-        writer.writerow({'ID': row[0], 'Quantity': row[1], 'Unit_price': row[2], 'Order_date': row[3], 'Customer': row[4], 'Product': row[5]})
+        writer.writerow({'ID': row[0], 'Quantity': row[1], 'Unit_price': row[2], 'Order_date': row[3], 'Customer': row[4], 'Product': row[5], 'Address': row[6]})
